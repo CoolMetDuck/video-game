419292a36d7de54892b901537cf330e6bddd4a31
CoolMetDuck <101306762+CoolMetDuck@users.noreply.github.com>
2022-06-22 18:19:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -677,7 +677,7 @@ function characterB(){
 function quit(){
 
 
-    window.close('http://127.0.0.1:5500/'); 
+    window.close('https://coolmetduck.github.io/video-game/DesertGame.html'); 
     
 }
 
